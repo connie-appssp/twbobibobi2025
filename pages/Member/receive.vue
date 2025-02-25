24f0cbca8ac7daa224e89d27cbdf3810bf1e9a50
connie-appssp <connie@appssp.com>
2025-02-25 15:32:26 +0800

    feat: 信眾專區-收件資料--新增收件資料UI

--- pages/Member/receive.vue
+++ pages/Member/receive.vue
@@ -10,58 +10,87 @@
 
         </div>
 
-        <h1 class="w-full text-center text-xl font-medium text-red-950 border-b pb-2 my-4 md:text-2xl">收件資料</h1>
+        <hr class="w-full border-0 h-px bg-zinc-200 my-5" />
 
-        <div class="w-fit mx-auto space-y-2">
+        <button class="w-full bg-red-950 text-white px-4 py-2 rounded-lg hover:bg-red-950/[.9] transition disabled:opacity-50"
+                v-on:click="openFormModal">新增收件資料</button>
 
-            <div class="w-fit flex items-center space-x-2">
-                <div class="w-20 text-justify" style="text-align-last: justify;">
-                    <label for="phone">收件人姓名</label>
-                </div>
-                <div class="w-60">
-                    <input type="text" v-model='input.name'
-                            class="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
-                            required>
-                </div>
-            </div>
-            
-            <div class="w-fit flex items-center space-x-2">
-                <div class="w-20 text-justify self-start pt-2" style="text-align-last: justify;">
-                    <label for="phone">收件地址</label>
-                </div>
-                <div class="w-60">
-                    <div class="w-full flex flex-row items-center">
-                        <input type="text" v-model='input.zip'
-                                class="w-1/3 p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
-                                placeholder="區碼"
-                                required>
-                        <select class="p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none">
-                            <option value="">縣市</option>
-                            <option value="">台中市</option>
-                        </select>
-                        <select class="p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none">
-                            <option value="">地區</option>
-                            <option value="">西屯區</option>
-                            <option value="">四個字區</option>
-                        </select>
 
+        <div v-if="showForm" class="fixed top-0 left-0 w-full h-dvh transition-all duration-300" ref="FormModal">
+            <div class="w-full h-dvh bg-black/25" ref="mask"></div>
+            <div class="absolute m-auto inset-0 h-fit w-fit max-w-full p-4">
+                <div class="bg-white w-full md:w-[500px] rounded-lg p-4">
+                    <div class="h-8 border-b flex justify-between pr-2" ref="modalHeader">
+                        <div>新增收件資料</div>
+                        <div class="text-2xl/4">
+                            <button v-on:click="showForm=false">×</button>
+                        </div>
                     </div>
-                    
-                    <input type="text" v-model='input.name'
-                            class="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
-                            placeholder="街道地址"
-                            required>
-                            <span class="text-zinc-400">請填寫完整地址，以便寄送特定服務所附之贈品</span>
+                    <div class="min-h-24 p-2 text-center content-center tracking-wider grid grid-cols-12 items-center space-y-1" ref="modalContent">
+                        
+                        <div class="col-span-4 md:col-span-3">
+                            <div class="px-2" style="text-align-last: justify;">收件人姓名</div>
+                        </div>
+                        <div class="col-span-8 md:col-span-9">
+                            <input type="text" v-model='input.name'
+                                    class="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                                    required>
+                        </div>
+
+                        <div class="col-span-12"></div>
+                        
+                        <div class="col-span-4 md:col-span-3">
+                            <div class="px-2" style="text-align-last: justify;">收件人電話</div>
+                        </div>
+                        <div class="col-span-8 md:col-span-9">
+                            <input type="tel" v-model='input.name'
+                                    class="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                                    required>
+                        </div>
+
+                        <div class="col-span-12"></div>
+                        
+                        <div class="col-span-4 md:col-span-3 self-start pt-2">
+                            <div class="px-2 " style="text-align-last: justify;">收件人地址</div>
+                        </div>
+                        <div class="col-span-12 md:col-span-9 space-y-1">
+                            <div class="w-full flex flex-row items-center">
+                                <input type="text" v-model='input.zip'
+                                        class="w-1/3 p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                                        placeholder="區碼"
+                                        required>
+                                <select class="w-1/3 p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none">
+                                    <option value="">縣市</option>
+                                    <option value="">台中市</option>
+                                </select>
+                                <select class="w-1/3 p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none">
+                                    <option value="">地區</option>
+                                    <option value="">西屯區</option>
+                                    <option value="">四個字區</option>
+                                </select>
+
+                            </div>
+                            
+                            <input type="text" v-model='input.name'
+                                    class="w-full p-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                                    placeholder="街道地址"
+                                    required>
+                                    <span class="text-zinc-400">請填寫完整地址，以便寄送特定服務之贈品</span>
+                        </div>
+                        
+                    </div>
+                    <div class="h-8 py-2 border-t flex justify-end" ref="modalFooter">
+                        <div>
+                            <button type="button" 
+                                class="bg-red-950 text-white px-2 py-1 rounded-lg hover:bg-yellow-950 transition disabled:opacity-50"
+                                ref="sendVerifyCodeBtn"
+                                v-on:click='verifyMember' disabled>
+                                儲存
+                            </button>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-
-            <button type="button" 
-                    class="w-full bg-red-950 text-white px-4 py-2 rounded-lg hover:bg-yellow-950 transition disabled:opacity-50"
-                    ref="sendVerifyCodeBtn"
-                    v-on:click='verifyMember' disabled>
-                儲存
-            </button>
 
         </div>
         
@@ -85,8 +114,7 @@ const authStore = {
 };
 
 const isSendVerifyCode = ref(false);
-// const authStore = useAuthStore()
-
+const showForm = ref(false);
 const define = {
     parseDefaultToIcon: (data) => {
         if (data === 1)
@@ -98,8 +126,6 @@ const define = {
 const input = reactive({
     name: authStore.user?.name,
     mobile: authStore.user?.mobile
-    // name: '',
-    // mobile: ''
 })
 
 let tableParams = null;
@@ -184,6 +210,12 @@ const generateTable = (params, dom) => {
     // $('#dataTablesButton').html(tableInst.button().container()); // buttons placement
 };
 
+const openFormModal = () => {
+    // initFormData();
+    showForm.value = true;
+}
+
+
 const handleLogout = () => {
     authStore.logout()
     useRouter().push('/Member')
@@ -202,6 +234,10 @@ onMounted(() => {
 
 </script>
 
-<style scoped>
+<style>
 @import "~/assets/css/plugins/dataTables.tailwindcss.css";
+
+.dt-info {
+    display: none;
+}
 </style>
